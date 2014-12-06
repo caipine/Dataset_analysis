f2c88378fb56f5acb0673e1541eebafbd17a5c78
caipine <caipine@users.noreply.github.com>
2014-12-05 19:51:08 -0500

    Update download_GSE3189.R
    library(utils)

--- GSE3189/download_GSE3189.R
+++ GSE3189/download_GSE3189.R
@@ -1,3 +1,5 @@
+library(utils)
+
 temp <- tempfile()
 download.file("http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?mode=raw&is_datatable=true&acc=GPL96&id=15653&db=GeoDb_blob82",temp)
 strs1 <- readLines(temp)
